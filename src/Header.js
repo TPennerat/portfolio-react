fee4f338ab1f60c1e2e59f0eba3a08b586296017
tpennerat <theo.pennerat@gmail.com>
2021-04-29 11:38:46 +0200

    navbar color and transition

--- src/Header.js
+++ src/Header.js
@@ -1,27 +1,20 @@
 import "./Header.css";
-import Nav from 'react-bootstrap/Nav';
 
-function Header () {
+function Header() {
     return (
-        <div className="Header">
- <Nav justify variant="tabs" className="justify-content-center" defaultActiveKey="/profil">
-  <Nav.Item>
-    <Nav.Link  href="/profil" >Profil</Nav.Link>
-  </Nav.Item>
-  <Nav.Item>
-    <Nav.Link key="test" eventKey="link-1" href="/skills" onSelect={() => this.selected(test)}>Compétences</Nav.Link>
-  </Nav.Item>
-  <Nav.Item>
-    <Nav.Link eventKey="link-2" href="/exp">Expériences</Nav.Link>
-  </Nav.Item>
-</Nav>
+        <div className="header">
+            <header className="main">
+                <nav className="navigation">
+                    <ul>
+                        <li><a href="#">Accueil</a></li>
+                        <li><a href="#">À propos</a></li>
+                        <li><a href="#">Contact</a></li>
+                    </ul>
+                </nav>
+            </header>
         </div>
     );
 }
 
-function selected(selectedKey) {
-  console.log('cc')
-  alert(selectedKey);
-}
 
 export default Header;
